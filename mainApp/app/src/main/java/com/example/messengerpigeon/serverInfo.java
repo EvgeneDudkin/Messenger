df4308d04078071eebda605d140e3de8b980d1e3
EvgeneDudkin <EvgeneMartyshenko@mail.ru>
2015-11-27 01:24:33 +0300

    qwe

--- mainApp/app/src/main/java/com/example/messengerpigeon/serverInfo.java
+++ mainApp/app/src/main/java/com/example/messengerpigeon/serverInfo.java
@@ -4,7 +4,7 @@ package com.example.messengerpigeon;
  * Created by Kirill2 on 29.10.2015.
  */
 public class serverInfo {
-    private static String IP = "217.197.0.29";
+    private static String IP = "172.20.234.242";
     private static int port = 3000;
 
     public static String getIP() {
