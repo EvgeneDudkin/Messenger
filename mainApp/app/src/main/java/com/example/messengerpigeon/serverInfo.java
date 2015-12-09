0960d48a9add414e324d610112dc5c22175e6aad
Egor Schavelev <egor5700@gmail.com>
2015-12-09 20:07:17 +0300

    Update serverInfo.java

--- mainApp/app/src/main/java/com/example/messengerpigeon/serverInfo.java
+++ mainApp/app/src/main/java/com/example/messengerpigeon/serverInfo.java
@@ -4,7 +4,6 @@ package com.example.messengerpigeon;
  * Created by Kirill2 on 29.10.2015.
  */
 public class serverInfo {
-    //private static String IP = "172.20.205.88";
     private static String IP = "172.20.205.88";
     private static int port = 3000;
 
